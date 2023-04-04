49c5f28ac34d9820458904e89a94a5b7e0bd76e2
Felipe <felipesand824@gmail.com>
2023-04-03 21:16:58 -0300

    about us

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Homepage from 'components/home';
 import './App.css';
 import Navbar from 'components/navbar';
+import AboutUs from 'components/about us';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
     <Navbar></Navbar>
     <Homepage></Homepage>
+    <AboutUs></AboutUs>
     </>
     
   );
---
+++ src/components/about us/index.tsx
@@ -0,0 +1,27 @@
+import Img2 from "assets/img/img2.svg";
+import './style.css';
+
+function AboutUs() {
+  return (
+    <div className="aboutUs-container">
+      <div className="aboutUs-img">
+      <img src={Img2} alt="Img2"></img>
+      </div>
+
+      <div className="aboutUs-content">
+        <h1>Sobre Nós</h1>
+        <p>
+          Somos uma equipe de seis jovens apaixonados por tecnologia e inovação,
+          que juntos uniram forças para criar uma ferramenta que mudasse a vida
+          de diversos profissionais. Acreditamos que o desenvolvimento de
+          software é uma arte e uma ciência - a arte de criar experiências
+          digitais excepcionais e a ciência de garantir que essas experiências
+          sejam eficientes. Combinamos nossas habilidades únicas para criar
+          soluções que não apenas funcionem bem, mas também pareçam incríveis.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
