dbd00eb5c344d13147930ab67c6d5a44e1a42ee8
Felipe <felipesand824@gmail.com>
2023-04-06 20:03:08 -0300

    Slider - Done

--- src/components/team/index.tsx
+++ src/components/team/index.tsx
@@ -7,13 +7,36 @@ import Slider from "react-slick";
 function Team(){
 
     const settings = {
-        dots: false,
-      infinite: true,
+      dots: false,
+      infinite: false,
       speed: 500,
       slidesToShow: 4,
       slidesToScroll: 4,
-      centerMode: true,
-      initialSlide: 2,
+      centerMode: false,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 1100,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
       };
 
 
@@ -21,13 +44,14 @@ function Team(){
         <>
         
         <div className='team-container' id='team'>
+          <h1 className='team-title'>TEAM</h1>
             <div className='team-carousel'>
             <Slider {...settings}>
                 <TeamCard name={'Felipe Sandes'} func={'Desenvolvedor Back-end'} img={John}></TeamCard>
                 <TeamCard name={'Kevin Cavalcanti'} func={'Desenvolvedor Mobile'} img={John}></TeamCard>
                 <TeamCard name={'Lucas Alves'} func={'Documentador'} img={John}></TeamCard>
                 <TeamCard name={'Murilo Santana'} func={'Desenvolvedor Front-end'} img={John}></TeamCard>
-                <TeamCard name={'Myrella Uchoa'} func={'Desenvolvedor Mobile'} img={John}></TeamCard>
+                <TeamCard name={'Myrella Uchoa'} func={'Desenvolvedora Mobile'} img={John}></TeamCard>
                 <TeamCard name={'Vinicius Rodrigues'} func={'Product Owner'} img={John}></TeamCard>
                 
             </Slider>
